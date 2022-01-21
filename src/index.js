0c86761e8a66149194475e1a71479819f43cb3b0
Vedanth Bora <vedanthbora@gmail.com>
2022-01-21 22:35:23 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+const app = document.getElementById("app");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  app
+);
